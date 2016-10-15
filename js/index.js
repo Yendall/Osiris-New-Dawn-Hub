83418017fcc52b26dc81b59ef3e962fc3e186a20
Yendall <yendallmax@gmail.com>
2016-10-15 18:02:40 +1100

    Asset Updates

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-// Mixing jQuery and Node.js code in the same file? Yes please!
+const remote = require('electron').remote
+const main = remote.require('./main.js')
 
-$(function(){
-
+$( () => {
     // Display some statistics about this computer, using node's os module.
-
     var os = require('os');
     var prettyBytes = require('pretty-bytes');
 
@@ -11,12 +10,10 @@ $(function(){
     $('.stats').append('Free memory: <span>' + prettyBytes(os.freemem())+ '</span>');
 
     // Electron's UI library. We will need it for later.
-
     var shell = require('shell');
     var categories = ['Inventory','Map','Building'];
 
-    // The same-origin security policy doesn't apply to electron, so we can
-    // send ajax request to other sites. Let's fetch Tutorialzine's rss feed:
-    
-
+    $('#building').click(() => {
+      main.openCrafting();
+    })
 });
--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const electron = require('electron')
-const {app, BrowserWindow} = electron
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+const {app,fs,BrowserWindow} = electron
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 625});
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+var win = null;
+// Called when Electron has finished
+// initialization and is ready to create all windows
+app.on('ready', () => {
+  // Create the index window
+  win = new BrowserWindow({width: 1000, height: 625});
+  // Load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`);
 });
+
+// Crafting interface
+exports.openCrafting = () => {
+  // Load the crafting.html of the app
+  win.loadURL(`file://${__dirname}/crafting.html`);
+}
+
+// Inventory interface
+exports.openInventory = () => {
+  // Load the inventory.html of the app
+  win.loadURL(`file://${__dirname}/inventory.html`);
+}
