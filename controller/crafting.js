69004eb8b454f1636fd6b23c25478ccf71b363be
Yendall <yendallmax@gmail.com>
2016-10-16 13:44:30 +1100

    table population

--- controller/crafting.js
+++ controller/crafting.js
@@ -1,66 +1,104 @@
 const remote = require('electron').remote;
 const main = remote.require('./main.js');
-// Fetch type of crafting
-var type = remote.getGlobal('craftingType');
-// Set image path
-var icon_path = '../model/assets/icons/';
-// Determine keys and model to populate list
-switch(type)
+
+// Start procedures
+main_func();
+
+function main_func()
 {
-  case 'vehicles':
-    var keys = ["vehicles"];
-    var model = remote.getGlobal('vehicles');
-    break;
-  case 'structures':
-    var keys = ["structures"];
-    var model = remote.getGlobal('structures');
-    break;
-  case 'craftables':
-    var keys = ["chemistry table","forge","fabricator"];
-    var model = remote.getGlobal('craftables');
-    break;
+  // Fetch type of crafting
+  var type = remote.getGlobal('craftingType');
+  // Set image path
+  var icon_path = '../model/assets/icons/';
+  // Determine keys and model to populate list
+  switch(type)
+  {
+    case 'vehicles':
+      var keys = ["vehicles"];
+      var model = remote.getGlobal('vehicles');
+      break;
+    case 'structures':
+      var keys = ["structures"];
+      var model = remote.getGlobal('structures');
+      break;
+    case 'craftables':
+      var keys = ["chemistry table","forge","fabricator"];
+      var model = remote.getGlobal('craftables');
+      break;
+  }
+  // Grab DOM
+  var tbody = document.getElementById("table_body");
+  // Craftables uses a different structure, so differentiate
+  if(type == 'craftables')
+  {
+    for(var i=0; i<keys.length; i++) {
+      for(var j=0; j<model[type][keys[i]].length; j++)
+      {
+        setupTable(tbody,model[type][keys[i]][j],type,icon_path);
+      }
+    }
+    console.log(tbody);
+  }else {
+    for(var i=0; i<keys.length; i++) {
+      for(var j=0; j<model[keys[i]].length; j++)
+      {
+        console.log(model[keys[i]][j].name);
+      }
+    }
+  }
 }
-// Grab DOM
-var tbody = document.getElementById("table_body");
-console.log(tbody);
-// Craftables uses a different structure, so differentiate
-if(type == 'craftables')
+
+function setupMaterials(tr,tbody,model,type,icon_path)
 {
-  for(var i=0; i<keys.length; i++) {
-    for(var j=0; j<model[type][keys[i]].length; j++)
-    {
-      // Set TR and TD elements
-      var tr = document.createElement('tr');
-      var td_name = document.createElement('td');
-      var td_material = document.createElement('td');
-      var p_name = document.createElement('p');
-      var p_material = document.createElement('p');
-      var img_name = document.createElement('img');
-      var span_name = document.createElement('span');
+  var td_mat = document.createElement('td');
+  td_mat.setAttribute('class','text-left');
+  console.log(model['materials'].length);
+  for(var i=0; i<model['materials'].length; i++)
+  {
+    console.log(i);
       var img_path =
-       type+'/Icon_'+model[type][keys[i]][j].name.replace(/\s/g,'')+'.png';
-      tbody.appendChild(tr);
+        type+'/Icon_'+model['materials'][i].replace(/\s/g,'')+'.png';
+      var p_mat = document.createElement('p');
+      var img_mat = document.createElement('img');
+      var span_mat = document.createElement('span');
+      p_mat.setAttribute('class','material-row');
+      img_mat.setAttribute('class','material-image');
+      img_mat.setAttribute('height','60');
+      img_mat.setAttribute('width','60');
+      img_mat.setAttribute('src',icon_path + img_path);
+      span_mat.setAttribute('class','material-text');
+      span_mat.innerHTML = model['amount'][i] + " x " + model['materials'][i];
+      p_mat.appendChild(img_mat);
+      p_mat.appendChild(span_mat);
+      td_mat.appendChild(p_mat);
 
-      td_name.setAttribute('class','text-left');
-      p_name.setAttribute('class','material_row');
-      img_name.setAttribute('class','material_image');
-      img_name.setAttribute('height','60');
-      img_name.setAttribute('width','60');
-      img_name.setAttribute('src',icon_path + img_path);
-      span_name.setAttribute('class','material_text');
-      span_name.innerHTML = model[type][keys[i]][j].name;
-      p_name.appendChild(img_name);
-      p_name.appendChild(span_name);
-      tbody.appendChild(td_name);
+  }
+  tr.appendChild(td_mat);
+  //console.log(tr);
+  tbody.appendChild(tr);
+}
 
-      console.log(model[type][keys[i]][j].name);
-    }
-  }
-}else {
-  for(var i=0; i<keys.length; i++) {
-    for(var j=0; j<model[keys[i]].length; j++)
-    {
-      console.log(model[keys[i]][j].name);
-    }
-  }
+function setupTable(tbody,model,type,icon_path)
+{
+  // Set TR and TD elements
+  var tr = document.createElement('tr');
+  var td_name = document.createElement('td');
+  var p_name = document.createElement('p');
+  var img_name = document.createElement('img');
+  var span_name = document.createElement('span');
+  var img_path =
+   type+'/Icon_'+model.name.replace(/\s/g,'')+'.png';
+  td_name.setAttribute('class','text-left');
+  p_name.setAttribute('class','material-row');
+  img_name.setAttribute('class','name-image');
+  img_name.setAttribute('height','60');
+  img_name.setAttribute('width','60');
+  img_name.setAttribute('src',icon_path + img_path);
+  span_name.setAttribute('class','material-text');
+  span_name.innerHTML = model.name;
+  p_name.appendChild(img_name);
+  p_name.appendChild(span_name);
+  td_name.appendChild(p_name);
+  tr.appendChild(td_name);
+  setupMaterials(tr,tbody,model,type,icon_path);
 }
